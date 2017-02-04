d43993fe8ca8228981723f720b5013e15c44aad6
Antonio Benavente del Moral <antonio@benavente-cardador.com>
2017-02-04 09:11:17 +0100

    Config connecto to mongodb with mongoose database pickandgol

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,6 +10,10 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//  Base de datos
+require('./lib/connectMongoose');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
