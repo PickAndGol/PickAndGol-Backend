df152bfb94a06f84f62ba348b54adbafc012d0d3
Edu González <fonologico@gmail.com>
2017-02-05 20:28:55 +0100

    Model Pub created. NewPub Api created. Access route in app.js modificated

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ let users = require('./routes/api/v1/users');
 app.use('/users', users.router);
 app.use('/users', users.jwtRouter);
 
+let pubs = require('./routes/api/v1/pubs');
+app.use('/pubs', pubs);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ models/Pub.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Edu on 5/2/17.
+ */
+
+"use strict";
+
+let mongoose = require('mongoose');
+let async = require('async');
+
+let pubSchema = mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+        index: true},
+    longitude: {
+        type: Number,
+        required: true},
+    latitude: {
+        type: Number,
+        required: true},
+    url: String,
+    photo_url: String,
+    owner_id: {
+        type: String,
+        required: true,
+        index: true}
+});
+
+pubSchema.statics.savePub = function (newPub, callback) {
+
+    let pub = new Pub (newPub);
+
+    pub.save(function (err, pub) {
+        if (err){
+            return callback(err);
+        }
+        return callback(err, pub);
+    })
+};
+
+pubSchema.statics.findPub = function (pubData, callback) {
+
+    //Buscamos por nombre.
+    Pub.findOne({name:pubData.name}).exec(function (err, pub) {
+        if (err){
+            return callback(err);
+        }
+        return callback(null, pub)
+    })
+};
+var Pub = mongoose.model('Pub',articleSchema);
