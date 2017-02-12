f4adeffb9fc563e68f8883818ad0c12ace844607
Alicia Daza <amdazacastillo@gmail.com>
2017-02-12 18:02:17 +0100

    Config -> SMTP config example

--- local_config_example.js
+++ local_config_example.js
@@ -1,5 +1,12 @@
 module.exports = {
-   jwt: {
-       secret: process.env.TOKEN_SECRET || '<TOKEN>'
-   }
+    jwt: {
+        secret: process.env.TOKEN_SECRET || '<TOKEN>'
+    },
+    smtpConfig: {
+        service: '<EMAIL_SERVICE>',
+        auth: {
+            user: '<USER_EMAIL>',
+            pass: '<EMAIL_PASSWORD>'
+        }
+    }
 };
