56a062a4fcc426c4ab7d7f2056cd78f96e9f60c0
Alicia Daza <amdazacastillo@gmail.com>
2017-02-18 22:54:45 +0100

    Return user id as _id

--- models/Users.js
+++ models/Users.js
@@ -177,7 +177,7 @@ UserPickSchema.statics.login = function(email, password) {
 
             resolve({
                 "token": token,
-                "id": user._id,
+                "_id": user._id,
                 "name": user.name,
                 "email": user.email,
                 "photo_url": user.photo_url,
